4c58ff45a7274667d47376f5b63271b14c966f90
louis0113 <luizhenrique011304@gmail.com>
2025-04-23 08:56:45 -0300

    Add bubble sort

--- Exercises/Beecrowd/Programas/sort.c
+++ Exercises/Beecrowd/Programas/sort.c
@@ -1,19 +1,48 @@
 #include <stdio.h>
 
-int main (void){
 
-  int a,b,c,x;
-  int numeros[3] = {a,b,c};
-
-  for(x = 0; x < 3; x++){
-    scanf("%d", &numeros[x]);
+void Sort(int arr[], int n){
+  int i, j, temp;
+  for(i = 0; i < n - 1; i++){
+    for(j = 0; j < n - i - 1; j++){
+      if(arr[j] > arr[j+1]){
+        temp = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = temp;
+      }
+    }
   }
+}
+
+int main (void){
   
-  for(x = 0; x < 3; x++){
+  int a,b,c,x,l;  
+  int numeros[3] = {a,b,c};
+  int sortedNums[3];
+
+   l = sizeof(numeros) / sizeof(numeros[0]);
+
+  for(x = 0; x < l; x++){
+    scanf("%d", &numeros[x]);
+    sortedNums[x] = numeros[x];
+  }
+
+  Sort(sortedNums, l);
+
+    for(x = 0; x < l; x++){
+
+    printf("%d\n", sortedNums[x]);
+    
+  }
+    puts("");
+    
+  for(x = 0; x < l; x++){
 
     printf("%d\n", numeros[x]);
     
   }
+  
+  
   
   return 0;
 
